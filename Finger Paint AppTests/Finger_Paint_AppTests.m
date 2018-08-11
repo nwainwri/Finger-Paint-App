b64c6ee7a41c1ca496b4b150cff4b744492f5204
Nathan Wainwright <nathanwainwright@gmail.com>
2018-08-10 22:39:33 -0700

    Initial Commit

---
+++ Finger Paint AppTests/Finger_Paint_AppTests.m
@@ -0,0 +1,39 @@
+//
+//  Finger_Paint_AppTests.m
+//  Finger Paint AppTests
+//
+//  Created by Nathan Wainwright on 2018-08-10.
+//  Copyright © 2018 Nathan Wainwright. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Finger_Paint_AppTests : XCTestCase
+
+@end
+
+@implementation Finger_Paint_AppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
